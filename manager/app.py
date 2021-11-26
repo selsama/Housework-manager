3dcbe41187348325d4e4034e7f876c98e15f5181
Sonja Salmi <salmi.sonjakaroliina@gmail.com>
2021-11-26 14:41:20 +0200

    database connection set up

--- manager/app.py
+++ manager/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
